471c3d4b576c0c1d29c1be422f4b5f144c254935
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-02 09:26:18 -0600

    Add reservations fetch call

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: []
+    }
+  }
+
+//   {
+// "id": 1,
+// "name": "Christie",
+// "date": "12/29",
+// "time": "7:00",
+// "number": 12
+// }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/reservations')
+      .then(res => res.json())
+      .then(data => this.setState({reservations: [...data]}))
+      .catch(err => console.error(err.message))
+  }
+
   render() {
     return (
       <div className="App">
