8886d94df4ef795b590df2a05147ff967320e694
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-02 09:49:47 -0600

    Add form labels and inputs and basic styling

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -5,14 +5,24 @@ class Form extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      name: null, 
+      date: null, 
+      time: null,
+      number: null
     }
   }
 
   render() {
     return(
       <form>
-
+        <label for='name'>Name:</label>
+        <input type='text' id='name' placeholder='your name' value={this.state.name} />
+        <label for='date'>Date:</label>
+        <input type='text' id='date' placeholder='date' value={this.state.date} />
+        <label for='time'>Time:</label>
+        <input type='text' id='time' placeholder='time' value={this.state.time} />
+        <label for='number'>Number of guests:</label>
+        <input type='text' id='number' placeholder='number of guests' value={this.state.number} />
       </form>
     )
   }
