7847c25b0bd4e9549de932e791071d9a96618a54
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-02 10:18:54 -0600

    Create makeRes method and add to button

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -16,6 +16,11 @@ class Form extends Component {
     this.setState({[e.target.id]: e.target.value})
   }
 
+  makeReservation() {
+    const newReservation = {...this.state, id: Date.now()}
+    this.props.addReservation(newReservation);
+  }
+
   render() {
     return(
       <form>
@@ -27,7 +32,7 @@ class Form extends Component {
         <input type='text' id='time' placeholder='time' value={this.state.time} onChange={(e) => this.updateState(e)} />
         <label htmlFor='number'>Number of guests:</label>
         <input type='text' id='number' placeholder='number of guests' value={this.state.number} onChange={(e) => this.updateState(e)} />
-        <button type='button'>Make Reservation</button>
+        <button type='button' onClick={() => this.makeReservation()}>Make Reservation</button>
       </form>
     )
   }
