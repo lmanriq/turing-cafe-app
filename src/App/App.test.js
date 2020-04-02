fc29b73671692b2b4e9e0d248b8e8625864ce7cc
Lili Manrique <liliana.manrique1@gmail.com>
2020-04-02 10:59:39 -0600

    Add integration test for adding a new reservation

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  it('should render what we expect', () => {
+    const { getByText } = render(
+      <App />
+    )
+    expect(getByText('Turing Cafe Reservations')).toBeInTheDocument();
+  });
+
+  it('should be able to add reservations', async () => {
+    const { getByText, getByPlaceholderText } = render(
+      <App />
+    )
+    const nameInput = getByPlaceholderText('your name');
+    const dateInput = getByPlaceholderText('date');
+    const timeInput = getByPlaceholderText('time');
+    const numberInput = getByPlaceholderText('number of guests');
+    const resBtn = getByText('Make Reservation');
+    fireEvent.change(nameInput, {target: {value: 'Lili'}});
+    fireEvent.change(dateInput, {target: {value: '7/20'}});
+    fireEvent.change(timeInput, {target: {value: '4:30'}});
+    fireEvent.change(numberInput, {target: {value: '5'}});
+    fireEvent.click(resBtn);
+    const newCardName = await getByText('Lili');
+    expect(newCardName).toBeInTheDocument();
+  })
 });
